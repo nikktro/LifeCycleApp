ae9deacfa8284eedb9d4154c744bf0f056f36a55
Nikolay Trofimov <nikktro@gmail.com>
2022-02-23 23:54:55 +0300

    Initial Commit

---
+++ LifeCycleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCycleApp
+//
+//  Created by Nikolay Trofimov on 23.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
