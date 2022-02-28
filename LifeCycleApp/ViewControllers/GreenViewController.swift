a84bea45e86988ee9431f3e2cf68ac80d0ea1308
Nikolay Trofimov <nikktro@gmail.com>
2022-03-01 00:00:34 +0300

    load view comment

--- LifeCycleApp/ViewControllers/GreenViewController.swift
+++ LifeCycleApp/ViewControllers/GreenViewController.swift
@@ -29,7 +29,7 @@ class GreenViewController: UIViewController {
         printMessage()
     }
     
-    //
+    // Manual view initialisation
     override func loadView() {
         super.loadView()
         printMessage()
