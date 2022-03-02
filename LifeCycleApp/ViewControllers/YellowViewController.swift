a0a9140c6bb0e8ce868d8d9f614d0f4e2f6f7119
Nikolay Trofimov <nikktro@gmail.com>
2022-03-02 23:48:11 +0300

    add life cycle methods to YellowViewController

--- LifeCycleApp/ViewControllers/YellowViewController.swift
+++ LifeCycleApp/ViewControllers/YellowViewController.swift
@@ -13,4 +13,36 @@ class YellowViewController: UIViewController {
         guard let greenVC = segue.destination as? GreenViewController else { return }
         greenVC.someProperty = "someProperty"
     }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        title = "Yellow VC"
+        printMessage()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        printMessage()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        printMessage()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        printMessage()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        printMessage()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        printMessage()
+    }
+    
 }
